56c7db98024a5042d0e547c8df465cb7245c122b
Mohammad Shaar <sharmhmd989@gmail.com>
2023-08-08 02:04:40 +0300

    new project

--- js/script.js
+++ js/script.js
@@ -1,31 +1,42 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    slidesPerView: 3,
-    spaceBetween: 25,
-    grabCursor: 'true',
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-      dynamicBullets: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+// Hamburger Icon
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
 
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        }
-    }
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
+
+// slide card
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: 3,
+  spaceBetween: 25,
+  grabCursor: "true",
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
 });
